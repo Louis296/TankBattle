635d1bafcb4e7c6eff735b84f0c9854a6dd5d1c4
Louis296 <1zhaozhiwei@gmail.com>
2019-12-12 10:29:37 +0800

    重构了代码体系，并修正了由getResource带来的空指针错误

---
+++ src/com/main/Main.java
@@ -0,0 +1,11 @@
+package com.main;
+
+import com.model.TankClient;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        new TankClient();
+    }
+}
---
+++ src/com/model/Direction.java
@@ -0,0 +1,5 @@
+package com.model;
+
+public enum Direction {
+	L, U,  R,  D,  STOP
+}
